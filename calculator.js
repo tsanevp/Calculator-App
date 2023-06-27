654ea9e3f327925a4afced6375a0312560b29a6e
tsanevp <petertsanev7@gmail.com>
2023-06-27 15:29:37 -0700

    refactored calculator.js and added documentation

--- calculator.js
+++ calculator.js
@@ -11,91 +11,121 @@ const operators = ['+', '-', '/', '*'];
 const decimal = '.';
 const clear = 'ac';
 const equals = '=';
+const initialDisplayValue = 0;
+const initialNumValue = "";
 
-let result = 0;
-let num1 = "";
-let num2 = "";
-let operator = '+';
-let operatorClicked = false;
+var result = initialDisplayValue;
+var num1 = initialNumValue;
+var num2 = initialNumValue;
+var operator = operators[0];
+var operatorClicked = false;
 
+/**
+ * Performs one of the following mathematical operations.
+ */
 var calculateAnswer = {
-    '+': function(x, y) {return x + y},
-    '-': function(x, y) {return x - y},
-    '/': function(x, y) {return x / y},
-    '*': function(x, y) {return x * y},
+    '+': function (x, y) { return x + y },
+    '-': function (x, y) { return x - y },
+    '/': function (x, y) { return x / y },
+    '*': function (x, y) { return x * y },
 };
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
     resetState();
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
     let buttonClicked = req.body.buttonClicked;
 
-    if (operators.includes(buttonClicked)) {
-        if (result !== 0) {
-            num1 = result;
-            num2 = "";
-        }
+    // Call function that handles calculator logic
+    calculatorLogic(buttonClicked);
 
-        operatorClicked = true;
-        operator = buttonClicked;
+    // Send number to display back to front end
+    res.json({ result });
+});
+
+/**
+ * Handles all the calculator logic.
+ * @param {String} buttonClicked - The button selected on the calculator.
+ */
+function calculatorLogic(buttonClicked) {
+    // AC clear button is clicked
+    if (buttonClicked === clear) resetState();
+
+    // Mathematical operator is clicked
+    if (operators.includes(buttonClicked)) operatorWasClicked(buttonClicked);
+
+    // Number is clicked after user has already solved for some value
+    if (result !== initialDisplayValue && num2 !== initialNumValue && !operatorClicked && buttonClicked !== equals) {
+        resetState();
     }
 
-    if (numbers.includes(buttonClicked)) {
-        let num = buttonClicked;
-        if (result !== 0 && num2 !== "" && !operatorClicked) {
-            resetState();
-        }
+    // Add number to current number
+    if (numbers.includes(buttonClicked)) appendNumber(buttonClicked);
 
-        if (operatorClicked) {
-            num2 += num;
-        } else {
-            num1 += num;
-        }
-    }
+    // Add decimal to current number
+    if (buttonClicked === decimal) appendDecimal(buttonClicked);
 
-    if (buttonClicked === decimal) {
-        let decimal = buttonClicked;
-        if (operatorClicked) {
-            num2 += decimal;
-        } else {
-            num1 += decimal;
-        }
-    } 
-
-    if (buttonClicked === equals) {
+    // Solve for some value
+    if (buttonClicked === equals && num1 !== initialNumValue && num2 !== initialNumValue) {
         result = calculateAnswer[operator](Number(num1), Number(num2));
         operatorClicked = false;
     }
 
-    if (num2 === "") {
-        result = num1;
-    }
-
-    if (num1 !== "" && num2 !== "" && operatorClicked) {
-        result = num2;
-    }
-
-    if (buttonClicked === clear) {
-        resetState();
-    }
-
-    res.json({ result });
-});
-
-function resetState () {
-    num1 = "";
-    num2 = "";
-    operatorClicked = false;
-    result = 0;
+    // Current number to display
+    if (num1 !== initialNumValue && num2 === initialNumValue) result = num1;
+    if (num1 !== initialNumValue && num2 !== initialNumValue && operatorClicked) result = num2;
 }
 
-app.listen('3000', function() {
+/**
+ * Resets the internal state of the calculator.
+ */
+function resetState() {
+    result = initialDisplayValue;
+    num1 = initialNumValue;
+    num2 = initialNumValue;
+    operatorClicked = false;
+}
+
+/**
+ * Handles logic for when a mathematical operator was clicked.
+ * @param {String} buttonClicked - The operator clicked.
+ */
+function operatorWasClicked(buttonClicked) {
+    if (result !== initialDisplayValue) {
+        num1 = result;
+        num2 = initialNumValue;
+    }
+
+    operatorClicked = true;
+    operator = buttonClicked;
+}
+
+/**
+ * Appends the number value of the button selected to the current number.
+ * @param {String} num - The numerical button value selected on the calculator.
+ */
+function appendNumber(num) {
+    operatorClicked ? num2 += num : num1 += num;
+}
+
+/**
+ * Appends a decimal to the current number IF the current number does not already contain a decimal.
+ * @param {String} decimal - The decimal to append to the current number.
+ */
+function appendDecimal(decimal) {
+    if (operatorClicked) {
+        num2.includes(decimal) ? {} : num2 += decimal;
+    } else {
+        num1.includes(decimal) ? {} : num1 += decimal;
+    }
+}
+
+app.listen('3000', function () {
     console.log("server is running on port 3000");
 });
